0a36a014b5f65c790d26170b4be99b598e25043f
raccoonjeong <rana3782@gmail.com>
2023-10-23 09:28:51 +0900

    Initial commit

---
+++ src/test/java/com/example/practivespringboot/PractiveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practivespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiveSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
